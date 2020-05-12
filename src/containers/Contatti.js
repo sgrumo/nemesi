6758d3fa0a54585ae02480893e1467ecbe26d20b
Samuele Medici <samuelemedici@Samueles-MBP.homenet.telecomitalia.it>
2020-05-12 14:54:57 +0200

    Added real texts

--- src/containers/Contatti.js
+++ src/containers/Contatti.js
@@ -10,9 +10,10 @@ export const Contatti = () => {
           <Card className="card-layout">
             <Card.Header as="h5">Maurizio Negroni</Card.Header>
             <Card.Body>
-              <Card.Title>Technical leader</Card.Title>
+              <Card.Title>Responsabile tecnico – CAD  Manager</Card.Title>
               <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
+                Vista la pluriennale esperienza nel settore della progettazione tecnica e in quella dei sistemi informativi tecnici è leader
+                tecnico per l'implementazione degli strumenti CAD, PDM/PLM, la loro personalizzazione, acquisti e formazione.
             </Card.Text>
             Per info contattare: <a href="tel:+393703242449">+39 3703242449</a>
             </Card.Body>
@@ -22,9 +23,11 @@ export const Contatti = () => {
           <Card className="card-layout">
             <Card.Header as="h5">Monica Medici</Card.Header>
             <Card.Body>
-              <Card.Title>Head of Business Management</Card.Title>
+              <Card.Title>Responsabile commerciale e amministrazione</Card.Title>
               <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
+                Anni di esperienza maturati prima nel settore commerciale, poi in quello amministrativo in qualità di direttore,
+                si occupa dello sviluppo e l’implementazione delle strategie commerciali, gestisce la rete vendita e i contatti con il cliente.
+                In ambito amministrativo supervisiona, organizza e coordina i vari servizi contabili e finanziari.
             </Card.Text>
               Per info contattare: <a href="tel:+393387550692">+39 3387550692</a>
             </Card.Body>
